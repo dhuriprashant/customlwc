fedb1b7a3398e5181f61bb1e821d87894a685289
Prashant Dhuri <dhuriprashant@gmail.com>
2023-12-12 19:13:08 -0700

    first commit

---
+++ force-app/main/default/lwc/myMapComponent/myMapComponent.js
@@ -0,0 +1,17 @@
+// myMapComponent.js
+import { LightningElement, track } from 'lwc';
+
+
+export default class MyMapComponent extends LightningElement {
+    myMap = {
+        'Key1': [{ name: 'Object1', value: 'Value1' }, { name: 'Object2', value: 'Value2' }],
+        'Key2': [{ name: 'Object3', value: 'Value3' }, { name: 'Object4', value: 'Value4' }]
+    };
+
+    @track myMapArray = Object.keys(this.myMap).map(key => ({key: key, value: this.myMap[key]}));
+
+    columns = [
+        { label: 'Name', fieldName: 'name' },
+        { label: 'Value', fieldName: 'value' }
+    ];
+}
