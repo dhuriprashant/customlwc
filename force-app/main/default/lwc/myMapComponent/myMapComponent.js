dee9ec1bc4cea4836e027381ba94fe42a15b51e1
Prashant Dhuri <dhuriprashant@gmail.com>
2024-01-03 19:30:22 -0700

    formatting

--- force-app/main/default/lwc/myMapComponent/myMapComponent.js
+++ force-app/main/default/lwc/myMapComponent/myMapComponent.js
@@ -1,17 +1,25 @@
 // myMapComponent.js
-import { LightningElement, track } from 'lwc';
-
+import { LightningElement, track } from "lwc";
 
 export default class MyMapComponent extends LightningElement {
-    myMap = {
-        'Key1': [{ name: 'Object1', value: 'Value1' }, { name: 'Object2', value: 'Value2' }],
-        'Key2': [{ name: 'Object3', value: 'Value3' }, { name: 'Object4', value: 'Value4' }]
-    };
+  myMap = {
+    Key1: [
+      { name: "Object1", value: "Value1" },
+      { name: "Object2", value: "Value2" }
+    ],
+    Key2: [
+      { name: "Object3", value: "Value3" },
+      { name: "Object4", value: "Value4" }
+    ]
+  };
 
-    @track myMapArray = Object.keys(this.myMap).map(key => ({key: key, value: this.myMap[key]}));
+  @track myMapArray = Object.keys(this.myMap).map((key) => ({
+    key: key,
+    value: this.myMap[key]
+  }));
 
-    columns = [
-        { label: 'Name', fieldName: 'name' },
-        { label: 'Value', fieldName: 'value' }
-    ];
+  columns = [
+    { label: "Name", fieldName: "name" },
+    { label: "Value", fieldName: "value" }
+  ];
 }
